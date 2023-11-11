a2b193afe83423b0b3291409777e0ea642650908
Nedko Arnaudov <nedko@nedk.org>
2023-11-11 20:27:07 +0200

    Revert "Gentoo libdir"
    This reverts commit 7002d9fc4a3f0c924ead2aadbe37d17aa4cebf31.

--- setup.py
+++ setup.py
@@ -519,7 +519,7 @@ class PyBuildExt(build_ext):
         global disable_ssl
         # Ensure that /usr/local is always used
         if not cross_compiling:
-            add_dir_to_list(self.compiler.library_dirs, '/usr/local/@@GENTOO_LIBDIR@@')
+            add_dir_to_list(self.compiler.library_dirs, '/usr/local/lib')
             add_dir_to_list(self.compiler.include_dirs, '/usr/local/include')
         if cross_compiling:
             self.add_gcc_paths()
@@ -586,7 +586,8 @@ class PyBuildExt(build_ext):
                 ):
                 add_dir_to_list(inc_dirs, d)
             for d in (
-                '/@@GENTOO_LIBDIR@@', '/usr/@@GENTOO_LIBDIR@@',
+                '/lib64', '/usr/lib64',
+                '/lib', '/usr/lib',
                 ):
                 add_dir_to_list(lib_dirs, d)
         exts = []
@@ -844,11 +845,11 @@ class PyBuildExt(build_ext):
             elif curses_library:
                 readline_libs.append(curses_library)
             elif self.compiler.find_library_file(lib_dirs +
-                                                     ['/usr/@@GENTOO_LIBDIR@@/termcap'],
+                                                     ['/usr/lib/termcap'],
                                                      'termcap'):
                 readline_libs.append('termcap')
             exts.append( Extension('readline', ['readline.c'],
-                                   library_dirs=['/usr/@@GENTOO_LIBDIR@@/termcap'],
+                                   library_dirs=['/usr/lib/termcap'],
                                    extra_link_args=readline_extra_link_args,
                                    libraries=readline_libs) )
         else:
@@ -1114,7 +1115,8 @@ class PyBuildExt(build_ext):
 
                 # check lib directories parallel to the location of the header
                 db_dirs_to_check = [
-                    db_incdir.replace("include", '@@GENTOO_LIBDIR@@'),
+                    db_incdir.replace("include", 'lib64'),
+                    db_incdir.replace("include", 'lib'),
                 ]
 
                 if host_platform != 'darwin':
@@ -1227,8 +1229,10 @@ class PyBuildExt(build_ext):
 
         if sqlite_incdir:
             sqlite_dirs_to_check = [
-                os.path.join(sqlite_incdir, '..', '@@GENTOO_LIBDIR@@'),
-                os.path.join(sqlite_incdir, '..', '..', '@@GENTOO_LIBDIR@@'),
+                os.path.join(sqlite_incdir, '..', 'lib64'),
+                os.path.join(sqlite_incdir, '..', 'lib'),
+                os.path.join(sqlite_incdir, '..', '..', 'lib64'),
+                os.path.join(sqlite_incdir, '..', '..', 'lib'),
             ]
             sqlite_libfile = self.compiler.find_library_file(
                                 sqlite_dirs_to_check + lib_dirs, 'sqlite3')
@@ -1997,14 +2001,15 @@ class PyBuildExt(build_ext):
             added_lib_dirs.append('/usr/openwin/lib')
         elif os.path.exists('/usr/X11R6/include'):
             include_dirs.append('/usr/X11R6/include')
-            added_lib_dirs.append('/usr/X11R6/@@GENTOO_LIBDIR@@')
+            added_lib_dirs.append('/usr/X11R6/lib64')
+            added_lib_dirs.append('/usr/X11R6/lib')
         elif os.path.exists('/usr/X11R5/include'):
             include_dirs.append('/usr/X11R5/include')
-            added_lib_dirs.append('/usr/X11R5/@@GENTOO_LIBDIR@@')
+            added_lib_dirs.append('/usr/X11R5/lib')
         else:
             # Assume default location for X11
             include_dirs.append('/usr/X11/include')
-            added_lib_dirs.append('/usr/X11/@@GENTOO_LIBDIR@@')
+            added_lib_dirs.append('/usr/X11/lib')
 
         # If Cygwin, then verify that X is installed before proceeding
         if host_platform == 'cygwin':
